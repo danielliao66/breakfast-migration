0c3f40b06c9c846f046157e293eaae8b89780d5a
unknown <danielliao66@gmail.com>
2026-07-15 21:10:53 +0800

    edit server url

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: "breakfast-server-production.up.railway.app"
+  apiUrl: "https://breakfast-server-production.up.railway.app"
 };
